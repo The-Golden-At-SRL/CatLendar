ba62b0e22c6910408116aae59214bc2543b17494
Jacopo Palumbo <jacopopalumbo@MacBook-Pro-di-Jacopo.local>
2024-05-06 23:28:00 +0200

    Now language is defined by the settings. Refactor of submit, now it works better

--- CatLendar.py
+++ CatLendar.py
@@ -1,12 +1,10 @@
 from pydantic import BaseModel
 
 import json
+
 from cat.experimental.form import form, CatForm, CatFormState
 from cat.log import log
 from .calendar import getAvailableDates, bookDate
-
-
-lang = "italian"
 
 class CalendarBooking(BaseModel):
     name: str
@@ -26,10 +24,13 @@ class CalendarBookingForm(CatForm):
     ]
     
     # It gives some problems with the current version
-    ask_confirm = False    
+    ask_confirm = False
         
     # Override because current version is experimental and provides a dev output
     def message(self):
+        # Get settings
+        settings = self.cat.mad_hatter.get_plugin().load_settings()
+        lang = settings["language"]
         
         if self._state == CatFormState.CLOSED:
             return {
@@ -59,7 +60,7 @@ class CalendarBookingForm(CatForm):
                 availableDates = getAvailableDates()
                 log.debug(availableDates)
 
-                availablePrompt = f"""Your task is to propose to the Human the availble dates for booking an appointment. You should ask the question in {lang}.
+                availablePrompt = f"""Your task is to propose to the user the availble dates for booking an appointment. You should ask the question in {lang}.
                 The available dates are the following:
                 ```json
                 {json.dumps(availableDates)}
@@ -75,7 +76,7 @@ class CalendarBookingForm(CatForm):
                 out = response
             else:    
                 # Here we want to generate a request phrase
-                prompt = f"""Your task is to ask the Human for an information. The information that you must ask is '{missing_fields_list[0]}'. You should ask the question in {lang}."""
+                prompt = f"""Your task is to ask the user for an information. The information that you must ask is '{missing_fields_list[0]}'. You should ask the question in {lang}."""
                 response = self.cat.llm(prompt)
                 log.debug(f"RESPONSE: {response}")
                 out = response
@@ -92,7 +93,7 @@ class CalendarBookingForm(CatForm):
     
         if self._state == CatFormState.WAIT_CONFIRM:
             # Generate confirm phrase
-            prompt = f"""Your task is to ask the Human to confirm the choosen date for the appointment. The Human shoul answer with yes or no.
+            prompt = f"""Your task is to ask the user to confirm the choosen date for the appointment. The user shoul answer with yes or no.
             The choosen date is '{self._model["bookingDate"]}'. You should ask the question in {lang}."""
             out = self.cat.llm(prompt)
 
@@ -100,18 +101,49 @@ class CalendarBookingForm(CatForm):
             "output": out
         }
     
-    def submit(self, form_data):        
+    def submit(self, form_data):
+        # Get settings
+        settings = self.cat.mad_hatter.get_plugin().load_settings()
+        lang = settings["language"]
+                
         # Separate date and hour from user result
-        datePrompt = f"""Your task is to print only the date in the format dd/mm/yyyy from the following string: '{form_data["bookingDate"]}'"""
+        availableDates = getAvailableDates()
+        datePrompt = f"""Your task is to produce a JSON representing the informations that the user has given to you.
+        JSON must be in this format:
+        ```json
+        {{
+            "date": // type string or null, must contain a date in the format dd/mm/yyyy
+            "time" // type string or null, must contain a time in the format hh:mm
+        }}
+        ```
+        The date and the time choosen by the user must be in the following JSON:
+        ```json
+        {json.dumps(availableDates)}
+        ```
+        
+        User said: "{form_data["bookingDate"]}"
+        
+        """
+        response = self.cat.llm(datePrompt)
+                
+        datePrompt = f"""Your task is to print only the date in the format dd/mm/yyyy from the following json: 
+        {response}
+        """
         choosendDate = self.cat.llm(datePrompt)
-        hourPrompt = f"""Your task is to print only the time in the format hh:mm from the following string: '{form_data["bookingDate"]}'"""
+        
+        hourPrompt = f"""Your task is to print only the time in the format hh:mm from the following json:
+        {response}
+        """
         choosenHour = self.cat.llm(hourPrompt)
+        
+        log.debug(choosendDate)
+        log.debug(choosenHour)
         
         # Book it
         bookDate(choosendDate, choosenHour, form_data["name"], form_data["email"], form_data["phoneNumber"])
         
         # Generate final phrase
-        prompt = f"""Your task is to tell the Human that his appointment has been booked. You should write the phrase in {lang}."""
+        prompt = f"""Your task is to tell the user that his appointment has been booked. You should write the phrase in {lang}."""
         out = self.cat.llm(prompt)
         
         return {
